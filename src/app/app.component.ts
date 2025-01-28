f75d9576e2438498f21379f5297ce5173395573e
Alejandro Martinez <mart.perez93@gmail.com>
2025-01-28 13:57:13 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CheckboxComponent } from "./checkbox/checkbox.component";
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-root',
+  imports: [CheckboxComponent, MatSlideToggleModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'budget';
+}
---
+++ src/app/checkbox/checkbox.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkbox',
+  imports: [],
+  templateUrl: './checkbox.component.html',
+  styleUrl: './checkbox.component.scss'
+})
+export class CheckboxComponent {
+
+}
