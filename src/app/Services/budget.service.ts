ef22bb8cd1994e6ff6918d03529ee532a994ea21
Alejandro Martinez <mart.perez93@gmail.com>
2025-02-11 15:11:13 +0100

    feat: add budget management functionality to BudgetService

--- src/app/Services/budget.service.ts
+++ src/app/Services/budget.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../interfaces/product';
 import { BehaviorSubject } from 'rxjs';
+import { Budget } from '../interfaces/budget';
 
 
 @Injectable({
@@ -8,7 +9,7 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class BudgetService {
 
-  private products : Product[] = [
+private products : Product[] = [
     {id: 1, title:'Seo', description: 'Hola', price: 300 },
     {id: 2, title:'Ads', description: 'Adios', price: 400 },
     {id: 3, title:'Web', description: 'Buenas', price: 500 }
@@ -16,7 +17,15 @@ export class BudgetService {
   
 private selectedProducts: { [key: number]: boolean } = {};
 
-arrayBudget: string[] = [];
+public Budgets: Budget[] = [{
+  name: 'Alejandro',
+  phone: 6756756756,
+  email: 'mart.perez93@gmail.com',
+  services:['Seo', 'Ads'],
+  total: 654
+}
+  
+];
 
 
 private numPages : number = 0;
@@ -64,6 +73,34 @@ getProduct() : Product []{
   return this.products;
 }
 
+addBudget(budgetData: Budget) {
+
+// Obtener los servicios seleccionados
+const selectedServices = this.products
+.filter(product => this.selectedProducts[product.id])
+.map(product => product.title);
+
+
+// Calcular el total
+const total = this.products.reduce(
+  (sum, product) => (this.selectedProducts[product.id] ? sum + product.price : sum),
+  this.calculateWeb()
+);
+
+// Crear el nuevo presupuesto
+const newBudget: Budget = {
+  ...budgetData, // Datos del formulario
+  services: selectedServices, // Servicios seleccionados
+  total: total // Total calculado
+};
+
+  this.Budgets.push(newBudget);
+  console.log("Presupuesto agregado:", newBudget);
+}
+
+getBudget(): Budget[]{
+  return this.Budgets;
+}
 
 
   constructor() {
