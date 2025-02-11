d7cb2b979d608177c82edab6a4c7900f13c5589d
Alejandro Martinez <mart.perez93@gmail.com>
2025-02-11 15:10:52 +0100

    feat: add form component with validation and budget submission functionality

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BudgetService } from '../Services/budget.service';
 
 @Component({
   selector:'app-form',
@@ -11,22 +12,36 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 export class FormComponent {
 resultado!: string;
 reactiveForm : FormGroup;
-data: string = "";
 
 
-constructor(private fb : FormBuilder) {
+@Output() data = new EventEmitter<any[]> ();
+
+constructor(private fb : FormBuilder, private budgetService: BudgetService) {
 this.reactiveForm = this.fb.group({
-name : ['', [Validators.required, Validators.minLength(10)]],
+name : ['', [Validators.required, Validators.minLength(3)]],
 phone: ['', [Validators.required, Validators.pattern(/^[0-9]{10}$/)]],
 email: ['', [Validators.required, Validators.email]],
 });
 }
 
 submit() {
-  if(this.reactiveForm.valid)
+  if(this.reactiveForm.valid){
   this.resultado = "Todos los datos son válidos";
-else
+
+  const budgetData = this.reactiveForm.value;
+  console.log(typeof budgetData)
+  console.log(budgetData)
+
+   this.budgetService.addBudget(budgetData);
+
+  this.data.emit(this.budgetService.getBudget());
+
+  this.reactiveForm.reset();
+
+} else{
   this.resultado = "Hay datos inválidos en el formulario";
+}
 }
 
 }
+
