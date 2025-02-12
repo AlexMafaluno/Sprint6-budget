c9a74ed994407a26c50628adee05ba3b3e483c4c
Alejandro Martinez <mart.perez93@gmail.com>
2025-02-12 10:47:28 +0100

    feat: refactor BudgetsListComponent to use reactive signals for budget loading

--- src/app/budgets-list/budgets-list.component.ts
+++ src/app/budgets-list/budgets-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, effect, inject, OnInit, Signal } from '@angular/core';
 import { BudgetService } from '../Services/budget.service';
 import { Budget } from '../interfaces/budget';
 import { CommonModule } from '@angular/common';
@@ -9,19 +9,21 @@ import { CommonModule } from '@angular/common';
   templateUrl: './budgets-list.component.html',
   styleUrl: './budgets-list.component.scss'
 })
-export class BudgetsListComponent implements OnInit {
+export class BudgetsListComponent {
 
 public budgets: Budget[]= [];
 
 private budgetService=inject(BudgetService);
 
 loadBudgets(){
-this.budgets = this.budgetService.getBudget();
+this.budgets = this.budgetService.Budgets();
 console.log('budget in budget-list-Component:', this.budgets);
 }
 
-ngOnInit() {
-  this.loadBudgets();
-}
+constructor() {
+effect(() => {
+  this.budgets = this.budgetService.Budgets();
+});
 
+}
 }
